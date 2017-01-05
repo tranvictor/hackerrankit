aa7c3d9fbee7ee1d820dc51b04a7773130ecbea3
tranvictor <vu.tran54@gmail.com>
2017-01-05 20:01:36 +0700

    using cpm rather than diff

--- template/C++/compile.sh
+++ template/C++/compile.sh
@@ -5,4 +5,4 @@ g++ main.cpp -o solution
 echo "Running your solution with sample input: sample.input. Output is directed to: my_result.output"
 ./solution < sample.input > my_result.output
 echo "Compare your result to sample.output"
-diff my_result.output sample.output
+cmp --silent my_result.output sample.output && echo "Result: Accepted" || echo "Result: Wrong Answer"
